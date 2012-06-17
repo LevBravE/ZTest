2840904fa7cf0485d4bc5fd4c6e71f6a65cda033
LevBravE <lev.brave@gmail.com>
2012-06-17 15:03:58 +0400

    Read style sheet and load translations

--- main.py
+++ main.py
@@ -7,7 +7,24 @@ import sys
 from PySide import QtCore, QtGui
 from MainWindow import MainWindow
 
+def readStyleSheet(app, fileName):
+    """
+    Загрузить файл стилей приложения
+    """
+    file = QtCore.QFile(fileName)
+    if not file.open(QtCore.QFile.ReadOnly | QtCore.QFile.Text):
+        return False
+    app.setStyleSheet(str(file.readAll()))
+    return True
+
 app = QtGui.QApplication(sys.argv)
+app.setWindowIcon(QtGui.QIcon('img/ztestlogo40x40.png'))
+
+readStyleSheet(app, 'qss/style.qss')
+translator = QtCore.QTranslator()
+if translator.load('qt_ru', 'translations'):
+    app.installTranslator(translator)
+
 QtCore.QTextCodec.setCodecForTr(QtCore.QTextCodec.codecForName('UTF-8'))
 mainWindows = MainWindow()
 mainWindows.show()
